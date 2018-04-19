e38b4c7a6b1cf47486bc306bd572e9684f4b5aed
elfish <32353376+elfish0@users.noreply.github.com>
2018-04-19 16:15:52 +0800

    Update Pascal's_triangle.py
    删除多余注释

--- generator/Pascal's_triangle.py
+++ generator/Pascal's_triangle.py
@@ -20,29 +20,3 @@ while True:
     except StopIteration as ex :
         print("generatoe return value: ",ex.value)
         break
-
-# 方法2:
-# def triangles():
-#     li = [1]
-#     while True:
-#         yield li
-#         li = [li[x]+li[x+1] for x in range(len(li)-1)]
-#         li.insert(0, 1)
-#         li.append(1)
-#
-# n = 0
-# results = []
-# for t in triangles():
-#     print(t)
-#     results.append(t)
-#     n = n + 1
-#     if n == 10:
-#         break
-
-
-
-
-
-
-
-
